23e3a59459014d4261355482811a905dbe61f3c8
Kota <shimizu.kota@cy-com.co.jp>
2026-06-12 15:43:46 +0900

    Staff完成

--- music-management/src/main/java/com/example/music_management/mapper/StaffMapper.java
+++ music-management/src/main/java/com/example/music_management/mapper/StaffMapper.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-
-import com.example.music_management.entity.Shop;
 import com.example.music_management.entity.Staff;
 
 @Mapper
