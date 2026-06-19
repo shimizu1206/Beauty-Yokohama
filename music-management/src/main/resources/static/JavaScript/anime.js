9ce4c189f9e811b8467897703ae41a254b7c600b
hiranoshu <hiranoshu@cy-com.co.jp>
2026-06-19 12:04:14 +0900

    あにめ中本さんありがとう

--- music-management/src/main/resources/static/JavaScript/anime.js
+++ music-management/src/main/resources/static/JavaScript/anime.js
@@ -2,9 +2,23 @@ const CLASSNAME = "-visible";
 const TIMEOUT = 1500;
 const $target = $(".title");
 
-setInterval(() => {
+// setInterval(() => {
+//   $target.addClass(CLASSNAME);
+//   setTimeout(() => {
+//     $target.removeClass(CLASSNAME);
+//   }, TIMEOUT);
+// }, TIMEOUT * 2);
+function animateOnce() {
   $target.addClass(CLASSNAME);
   setTimeout(() => {
     $target.removeClass(CLASSNAME);
   }, TIMEOUT);
-}, TIMEOUT * 2);
+}
+
+animateOnce();
+
+setTimeout(() => {
+  $("#loader").fadeOut(0, () => {
+    $("#content").show();
+  });
+}, 3000);
